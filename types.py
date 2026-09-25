47e1273554c65c73b0c55da9f9a8618f45e6d02d
theekhayusoley <theekhayusoleysrisawat@gmail.com>
2026-09-25 10:46:46 +0200

    Create types.py

---
+++ types.py
@@ -0,0 +1,16 @@
+###
+# Exercicis - types()
+# Completa els exercicis següents utilitzant la funció type()
+###
+
+# Exercici 1
+# Imprimeix el tipus del número 25
+print(type(25))
+
+# Exercici 2
+# Imprimeix el tipus del text "Hola món"
+print(type("Hola món"))
+
+# Exercici 3
+# Imprimeix el tipus del resultat de la comparació 10 > 5
+print(type(10>5))
