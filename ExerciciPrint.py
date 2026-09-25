c01d9cac6c1f3d72f67855e0509c98cecfffa68f
theekhayusoley <theekhayusoleysrisawat@gmail.com>
2026-09-25 10:18:21 +0200

    Update ExerciciPrint.py

--- ExerciciPrint.py
+++ ExerciciPrint.py
@@ -11,7 +11,7 @@ print("Em dic Theekhayu Soley Srisawat")
 # Exercici 2
 # Imprimeix les paraules "Python", "és", "divertit" separades per un guió (-)
 # Pista: utilitza el paràmetre 'sep' de print()
-print("python - es - divertit")
+print("Python", "és", "divertit", sep="-")
 
 # Exercici 3
 # Imprimeix el resultat de la suma 15 + 27
