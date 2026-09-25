87d822dcfe8331174d35d62aa5bc67dd5d841524
theekhayusoley <theekhayusoleysrisawat@gmail.com>
2026-09-25 10:09:03 +0200

    Create ExerciciPrint.py

---
+++ ExerciciPrint.py
@@ -0,0 +1,20 @@
+###
+# Exercicis - print()
+# Completa els exercicis següents utilitzant la funció print()
+###
+
+# Exercici 1
+# Imprimeix el teu nom i cognom a la consola
+print("Em dic Theekhayu Soley Srisawat")
+
+
+# Exercici 2
+# Imprimeix les paraules "Python", "és", "divertit" separades per un guió (-)
+# Pista: utilitza el paràmetre 'sep' de print()
+print("python - es - divertit")
+
+# Exercici 3
+# Imprimeix el resultat de la suma 15 + 27
+a=15
+b=27
+print(a+b)
